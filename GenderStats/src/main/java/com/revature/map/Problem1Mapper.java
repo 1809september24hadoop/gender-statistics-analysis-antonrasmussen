39f90b0fea4d333c80eac02ad5b08b1f737005fe
cloudera <cloudera@quickstart.cloudera>
2018-10-28 18:10:46 -0700

    Clean up Problem 1

--- GenderStats/src/main/java/com/revature/map/Problem1Mapper.java
+++ GenderStats/src/main/java/com/revature/map/Problem1Mapper.java
@@ -7,67 +7,64 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-
-/* 
- * To define a map function for your MapReduce job, subclass 
- * the Mapper class and override the map method.
- * The class definition requires four parameters: 
- *   The data type of the input key
- *   The data type of the input value
- *   The data type of the output key (which is the input key type 
- *   for the reducer)
- *   The data type of the output value (which is the input value 
- *   type for the reducer)
- */
-
 /**
  * PROBLEM #1: Identify the countries where % of female graduates is less than 30%. 
- * @author cloudera
+ *
+ * 
+ * @author Anton Rasmussen
  *
  */
 
 public class Problem1Mapper extends Mapper<LongWritable, Text, Text, DoubleWritable> {
 	
-	/*
-	 * The map method runs once for each line of text in the input file.
-	 * The method receives a key of type LongWritable, a value of type
-	 * Text, and a Context object.
-	 */
+	public static final String HEADER_ROW_TOKEN = "Country Name";
+	public static final String CSV_SPLITTER = "\",\"";
+
+	public static final String CSV_LAST_COL = "\",";
+	public static final String REPLACE_WITH = "0";
+	
+	//Educational attainment, at least completed upper secondary, 
+	//population 25+, female (%) (cumulative)
+	public static final String INDICATOR_CODE = "SE.SEC.CUAT.UP.FE.ZS";
+	
+	public static final int FIRST_YEAR = 4; // Col = 1960	
+	public static final int LAST_YEAR = 60; // Col = 2016
+
+
 	@Override
 	public void map(LongWritable key, Text value, Context context)
 			throws IOException, InterruptedException {
 
 		//Filter out Header Row
-		if(!value.toString().contains("Country Name")) {
+		if(!value.toString().contains(HEADER_ROW_TOKEN)) {
 
-			String[] row = value.toString().trim().split("\",\"");
+			String[] row = value.toString().trim().split(CSV_SPLITTER);
 			String outputKey = row[0]; //Country Name
 			String indicatorCode = row[3]; //For filtering
-			String lastColumn = row[60]; //Last Year Col = 2016
+			String lastColumn = row[LAST_YEAR]; //Last Year Col = 2016
 			
-			Double lastColDub = Double.parseDouble(lastColumn.replace("\",", "0"));
+			Double lastColDub = Double.parseDouble(
+					lastColumn.replace(CSV_LAST_COL, REPLACE_WITH));
+			
 			lastColumn = lastColDub.toString();
 		
-			double yearPercent = new Double(0.00);
+			double yearPercent = new Double(0);
 			
 			//Considering countries where the percentage of female graduates 
 			//constitutes at least completing upper secondary
-			if(indicatorCode.contains("SE.SEC.CUAT.UP.FE.ZS"))
+			if(indicatorCode.equals(INDICATOR_CODE))
 			{
-				for (int i = 4; i < 60; i++){
+				for (int i = FIRST_YEAR; i < LAST_YEAR; i++){
 					if(!row[i].isEmpty())
 					{
 						//Get most recent year's cumulative %
 						yearPercent = new Double(row[i]);
-						
-						//TODO: It might be more interesting to
-						//Calculate for average over time
-						//yearPercentTotals += new Double(row[i]);
-						//numOfYears++;
+
 					}
 					//Check for values >= 2 digits and remove the trailing ",
 					if(lastColDub > 0 && lastColumn.length() >= 2){
-						yearPercent = new Double(lastColumn.substring(0, lastColumn.length() - 2));
+						yearPercent = new Double(
+								lastColumn.substring(0, lastColumn.length() - 2));
 					}
 					//Check for values = 1 digit and remove the trailing ",
 					else if(lastColDub > 0) {
@@ -75,7 +72,8 @@ public class Problem1Mapper extends Mapper<LongWritable, Text, Text, DoubleWrita
 					}
 				}
 
-				context.write(new Text(outputKey.substring(1)), new DoubleWritable(yearPercent));
+				context.write(new Text(outputKey.substring(1)), 
+						      new DoubleWritable(yearPercent));
 			}
 		}
 	}
--- GenderStats/src/main/java/com/revature/reduce/Problem1Reducer.java
+++ GenderStats/src/main/java/com/revature/reduce/Problem1Reducer.java
@@ -7,28 +7,11 @@ import java.text.NumberFormat;
 import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
-
-/* 
- * To define a reduce function for your MapReduce job, subclass 
- * the Reducer class and override the reduce method.
- * The class definition requires four parameters: 
- *   The data type of the input key (which is the output key type 
- *   from the mapper)
- *   The data type of the input value (which is the output value 
- *   type from the mapper)
- *   The data type of the output key
- *   The data type of the output value
- */   
+ 
 public class Problem1Reducer extends Reducer<Text, DoubleWritable, Text, DoubleWritable> {
 	
 	NumberFormat formatter = new DecimalFormat("#0.00");
 	
-	/*
-	 * The reduce method runs once for each key received from
-	 * the shuffle and sort phase of the MapReduce framework.
-	 * The method receives a key of type Text, a set of values of type
-	 * IntWritable, and a Context object.
-	 */
 	@Override
 	public void reduce(Text key, Iterable<DoubleWritable> mostCurrentYear, Context context)
 			throws IOException, InterruptedException {
